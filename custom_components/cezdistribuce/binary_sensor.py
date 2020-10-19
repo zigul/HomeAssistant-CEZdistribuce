85fb24574531f1d095cf934174d89657bbdfccba
Jan Vakula <jan.vakula@datavision.cz>
2020-10-19 14:57:34 +0200

    Added implementation of simple binary_sensor

---
+++ custom_components/cezdistribuce/binary_sensor.py
@@ -0,0 +1,91 @@
+__version__ = "0.1"
+
+import logging
+from . import downloader
+import voluptuous as vol
+from datetime import timedelta, datetime, date
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.util import Throttle
+
+import requests
+from lxml import html, etree
+
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=3600)
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "cezdistribuce"
+CONF_REGION = "region"
+CONF_CODE = "code"
+CONF_NAME = "name"
+
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_REGION): cv.string,
+        vol.Required(CONF_CODE): cv.string,
+        vol.Required(CONF_NAME): cv.string,
+    }
+)
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    name = config.get(CONF_NAME)
+    region = config.get(CONF_REGION)
+    code = config.get(CONF_CODE)
+
+    ents = []
+    ents.append(CezDistribuce(name,region,code))
+    add_entities(ents)
+
+class CezDistribuce(BinarySensorEntity):
+
+    def __init__(self, name, region, code):
+        """Initialize the sensor."""
+        self._name = name
+        self.region = region
+        self.code = code
+        self.responseJson = "[]"
+        self.update()
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return "mdi:power"
+
+    @property
+    def is_on(self):
+        return downloader.isHdo(self.responseJson)
+
+    @property
+    def device_state_attributes(self):
+        attributes = {}
+        attributes['response_json'] = self.responseJson
+        return attributes
+
+    @property
+    def should_poll(self):
+        return True
+
+    @property
+    def available(self):
+        return self.last_update_success
+
+    @property
+    def device_class(self):
+        return ''
+
+    @Throttle(MIN_TIME_BETWEEN_SCANS)
+    def update(self):
+        # REGION = "regionStred"
+        # CODE = "A1B5DP6"
+
+        response = requests.get(downloader.getRequestUrl(self.region,self.code))
+        if response.status_code == 200:
+            self.responseJson = response.json()
+            self.last_update_success = True
+        else:
+            self.last_update_success = False
---
+++ custom_components/cezdistribuce/downloader.py
@@ -0,0 +1,37 @@
+import requests
+import datetime
+
+BASE_URL = "https://www.cezdistribuce.cz/edee/content/sysutf/ds3/data/hdo_data.json"
+
+def getRequestUrl(region, code):
+    return BASE_URL + "?&code=" + code + "&"+ region +"=1"
+
+def timeInRange(start, end, x):
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
+def parseTime(date_time_str):
+    if(not date_time_str):
+        return datetime.time(0,0)
+    else:
+        return datetime.datetime.strptime(date_time_str, '%H:%M').time()
+
+def parseDate(date_time_str):
+    return datetime.datetime.strptime(date_time_str,'%Y-%m-%d %H:%M:%S.%f')
+
+def isHdo(jsonCalendar):
+    now = datetime.datetime.now()
+    if(now.weekday()>4):
+        days = jsonCalendar[0]
+    else:
+        days = jsonCalendar[1]
+
+    actualTime = now.time()
+    hdo = False
+    for i in range(1,11):
+        startTime = parseTime(days["casZap" + str(i)])
+        endTime = parseTime(days["casVyp" + str(i)])
+        hdo = hdo or timeInRange(startTime, endTime, actualTime)
+    return hdo
