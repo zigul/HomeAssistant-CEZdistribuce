b541e7651595a84f04cc207b071a3481843c23a8
Michal Halenka <michal.halenka@gmail.com>
2021-11-20 02:33:45 +0100

    Fix timezone issue

--- custom_components/cezdistribuce/downloader.py
+++ custom_components/cezdistribuce/downloader.py
@@ -1,7 +1,16 @@
 import requests
 import datetime
 
+try:
+    # python 3.9+
+    from zoneinfo import ZoneInfo
+except ImportError:
+    # python 3.6-3.8
+    from backports.zoneinfo import ZoneInfo
+
+
 BASE_URL = "https://www.cezdistribuce.cz/distHdo/adam/containers/"
+CEZ_TIMEZONE = ZoneInfo("Europe/Prague")
 
 
 def getCorrectRegionName(region):
@@ -34,17 +43,31 @@ def parseDate(date_time_str):
     return datetime.datetime.strptime(date_time_str, "%Y-%m-%d %H:%M:%S.%f")
 
 
-def isHdo(jsonCalendar):
-    now = datetime.datetime.now()
-    if now.weekday() < 5:
-        days = jsonCalendar[0]
-    else:
-        days = jsonCalendar[1]
+def isHdo(jsonCalendar, daytime=datetime.datetime.utcnow()):
+    """
+    Find out if the HDO is enabled for the current timestamp
 
-    actualTime = now.time()
+    :param jsonCalendar: JSON with calendar schedule from CEZ
+    :param daytime: relevant time in UTC to check if HDO is on or not
+    :return: bool
+    """
+    # convert daytime to timezone CEZ is using on website
+    daytime.replace(tzinfo=ZoneInfo("UTC"))
+    daytime = daytime.astimezone(CEZ_TIMEZONE)
+    daytime.replace(tzinfo=None)
+
+    # select Mon-Fri schedule or Sat-Sun schedule according to current date
+    if daytime.weekday() < 5:
+        dayCalendar = jsonCalendar[0]
+    else:
+        dayCalendar = jsonCalendar[1]
+
+    checkedTime = daytime.time()
     hdo = False
+
+    # iterate over scheduled times in calendar schedule
     for i in range(1, 11):
-        startTime = parseTime(days["CAS_ZAP_" + str(i)])
-        endTime = parseTime(days["CAS_VYP_" + str(i)])
-        hdo = hdo or timeInRange(startTime, endTime, actualTime)
+        startTime = parseTime(dayCalendar["CAS_ZAP_" + str(i)])
+        endTime = parseTime(dayCalendar["CAS_VYP_" + str(i)])
+        hdo = hdo or timeInRange(start=startTime, end=endTime, x=checkedTime)
     return hdo
