da2dac3ace8a2c6fef6216029466d8e634ee5e41
Jan Vakula <jan@vakula.cz>
2022-03-07 21:13:17 +0100

    Update binary_sensor.py

--- custom_components/cezdistribuce/binary_sensor.py
+++ custom_components/cezdistribuce/binary_sensor.py
@@ -81,7 +81,7 @@ class CezDistribuce(BinarySensorEntity):
 
     @property
     def unique_id(self):
-        return "cezdistribuce" + self._name
+        return "cezdistribuce_" + self._name
 
     @Throttle(MIN_TIME_BETWEEN_SCANS)
     def update(self):
